f5fcacf6ee597e74eef8618c83f38d2d634d3687
Gustavo <gustavotobiasp@gmail.com>
2024-12-21 02:05:16 -0300

    Proyecto X de Eddy

---
+++ ElBuenAhorcado/src/main/java/Ed-Edd-y-Eddy/App.java
@@ -0,0 +1,13 @@
+package Ed-Edd-y-Eddy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
